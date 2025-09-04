cb24a43e171d6dd810b41c7b1f6ad2bf1765b08b
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-09-04 17:29:01 +0300

    feat: implement functions for loading graph by name, getting its metadata, creating two cycled graph by number of nodes and labels, saving graph to dot
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

---
+++ project/task1.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple
+from dataclasses import dataclass
+import cfpq_data as cfpq
+from networkx import MultiDiGraph
+from pyparsing import Any
+from networkx.drawing.nx_pydot import write_dot
+
+@dataclass
+class GraphMetaData:
+    nodes_number: int
+    edges_number: int
+    labels: List[Any]
+
+def load_graph_by_name(graph_name: str) -> MultiDiGraph:
+    graph_path = cfpq.download(graph_name)
+    return cfpq.graph_from_csv(graph_path)
+
+def get_graph_meta_data(graph_name: str) -> GraphMetaData:
+    graph = load_graph_by_name(graph_name)
+    return GraphMetaData(
+        nodes_number=cfpq.graph.number_of_nodes(),
+        edges_number=cfpq.graph.number_of_edges(),
+        labels=cfpq.get_sorted_labels(graph)
+    )
+
+def create_two_cycled_graph(fst_c_node_num: int, snd_c_node_num: int, labels: Tuple[str, str]) -> MultiDiGraph:
+    return cfpq.labeled_two_cycles_graph(fst_c_node_num, snd_c_node_num, labels=labels)
+
+def save_graph_to_dot(graph: MultiDiGraph, path: str):
+    write_dot(graph, path)
+
+#g = create_two_cycled_graph(4, 5, ("pipi", "pupu"))
+#save_graph_to_dot(g, "hi.dot")
