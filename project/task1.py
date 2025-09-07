38a6cc07aa7d5eb1f7305dec22e73b4f9f9188f9
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-09-07 12:42:39 +0300

    ref: change task1 organization for better logic
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- project/task1.py
+++ project/task1.py
@@ -1,9 +1,10 @@
 from typing import List, Tuple
 from dataclasses import dataclass
-import cfpq_data as cfpq
 from networkx import MultiDiGraph
 from pyparsing import Any
 from networkx.drawing.nx_pydot import write_dot
+
+import cfpq_data as cfpq
 
 
 @dataclass
@@ -18,20 +19,21 @@ def _load_graph_by_name(graph_name: str) -> MultiDiGraph:
     return cfpq.graph_from_csv(graph_path)
 
 
+def _save_graph_to_dot(graph: MultiDiGraph, path: str):
+    write_dot(graph, path)
+
+
 def get_graph_meta_data(graph_name: str) -> GraphMetaData:
     graph = _load_graph_by_name(graph_name)
     return GraphMetaData(
-        nodes_number=cfpq.graph.number_of_nodes(),
-        edges_number=cfpq.graph.number_of_edges(),
-        labels=cfpq.get_sorted_labels(graph),
+        graph.number_of_nodes(),
+        graph.number_of_edges(),
+        cfpq.get_sorted_labels(graph),
     )
 
 
-def create_two_cycled_graph(
-    fst_c_node_num: int, snd_c_node_num: int, labels: Tuple[str, str]
-) -> MultiDiGraph:
-    return cfpq.labeled_two_cycles_graph(fst_c_node_num, snd_c_node_num, labels=labels)
-
-
-def save_graph_to_dot(graph: MultiDiGraph, path: str):
-    write_dot(graph, path)
+def create_two_cycles_graph_and_save_to_dot(
+    fst_c_node_num: int, snd_c_node_num: int, labels: Tuple[str, str], file_path: str
+):
+    graph = cfpq.labeled_two_cycles_graph(fst_c_node_num, snd_c_node_num, labels=labels)
+    _save_graph_to_dot(graph, file_path)
